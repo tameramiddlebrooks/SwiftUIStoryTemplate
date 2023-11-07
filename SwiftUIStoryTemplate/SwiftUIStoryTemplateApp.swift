7e9a974b02100b41bb778002c18d64bb99bc192c
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2023-11-07 16:28:02 -0500

    Added project files

---
+++ SwiftUIStoryTemplate/SwiftUIStoryTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStoryTemplateApp.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Zoe Cutler on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStoryTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
