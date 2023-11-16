7005e6212a1445541b1a9280cc73744caf76f5b0
Shante <spatillo23@msu.idserve.net>
2023-11-16 15:21:46 -0500

    CHANGED BACKGROUND COLOR

--- SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
+++ SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct Chapter5View: View {
     var body: some View {
         Text("Chapter 5")
+        
+            Color(red: 0.437, green: 0.873, blue: 0.61).ignoresSafeArea()
+        
     }
 }
 
