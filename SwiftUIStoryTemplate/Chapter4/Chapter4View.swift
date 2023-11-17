13c79e67fd37770376bdb2cbcf0b7c7fb8b9296d
Thailey1 <haileytommy3@gmail.com>
2023-11-17 14:11:42 -0500

    Added Chapter4 View

--- SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
+++ SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
@@ -1,11 +1,54 @@
 import SwiftUI
 
 struct Chapter4View: View {
+    // Emotions array
+    let mixOfEmotions = ["Excited", "Determined", "Curious", "Joyful"]
+    
+    // Boolean variable to indicate whether good vibes have formed
+    let goodVibesFormed = true // or false, depending on your scenario
+    
     var body: some View {
-        Text("Chapter 4")
+//        NavigationView{
+            ZStack {
+                Color.shadeOfGreen
+                    .edgesIgnoringSafeArea(.all)
+                
+                ScrollView {
+                    // Narrative
+                    Text("Once upon a time at the Apple Developer Academy, a group of aspiring junior iOS developers embarked on a long and challenging journey.")
+                    Text("They came from diverse backgrounds, each with their own unique experiences and talents.")
+                    Text("As they gathered for their first day at the academy, there was a mix of excitement and determination held back by chill vibes roaming in the air.")
+                    
+                    // Emotions array
+                    ForEach(mixOfEmotions, id: \.self) { emotion in
+                        Text(emotion)
+                    }
+                    
+                    // Good vibes check
+                    if goodVibesFormed {
+                        Text("Despite the challenges they all faced individually, the group managed to overcome them and were picked randomly by the academy to form good vibes.")
+                    } else {
+                        Text("The journey continued, and the group works towards forming good vibes.")
+                    }
+                    
+                    // Additional content goes here
+                    
+                }
+                .navigationTitle("Chapter 4")
+                .navigationBarTitleDisplayMode(.automatic)
+                .navigationBarItems(
+                    leading: Image(systemName: "person.fill"),
+                    trailing: NavigationLink(destination: ContentView(), label: {
+                        Image(systemName: "gear")
+                    })
+                )
+            }
+        }
     }
-}
+//}
 
-#Preview {
-    Chapter4View()
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        Chapter4View()
+    }
 }
